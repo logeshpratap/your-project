d2d48c4332d792d2badf0c531c516fff7f05f907
Logesh Pratap M <128682838+logeshpratap@users.noreply.github.com>
2025-11-24 17:48:45 +0530

    Update test_app.py

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,1 +1,11 @@
 # test.py
+from src.app import add, subtract, multiply
+
+def test_add():
+    assert add(2, 3) == 5
+
+def test_subtract():
+    assert subtract(5, 3) == 2
+
+def test_multiply():
+    assert multiply(4, 3) == 12
